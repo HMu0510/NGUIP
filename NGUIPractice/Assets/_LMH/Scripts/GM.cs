8ba9ddfa40a01bb6946ad68b4be5769449bd7338
HMu0510 <HMu0510@gmail.com>
2020-07-15 12:27:29 +0900

    NGUI BackGround Set
    - Add Dam
    - Sprite Detail Border Set(Image's Edge 1Pixel Error Fix)

---
+++ NGUIPractice/Assets/_LMH/Scripts/GM.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    [SerializeField] private GameObject bgSetObj;
+    [SerializeField] private GameObject bgSetObj2;
+
+
+    public float moveSpeed;
+    public float moveSpeed2;
+
+    private float xPosMoveCheck = 0f;
+    private float xPosMoveCheck2 = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xPosMoveCheck -= moveSpeed * Time.deltaTime;
+        xPosMoveCheck2 -= moveSpeed2 * Time.deltaTime;
+        bgSetObj.transform.localPosition -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        bgSetObj2.transform.localPosition -= new Vector3(moveSpeed2 * Time.deltaTime, 0, 0);
+
+        if (xPosMoveCheck <= -960)
+        {
+            xPosMoveCheck = 0;
+            bgSetObj.transform.localPosition = new Vector3(960 * -1.0f, 0, 0);
+        }
+        if (xPosMoveCheck2 <= -960)
+        {
+            xPosMoveCheck2 = 0;
+            bgSetObj2.transform.localPosition = new Vector3(960 * -1.0f, -400, 0);
+        }
+    }
+}
