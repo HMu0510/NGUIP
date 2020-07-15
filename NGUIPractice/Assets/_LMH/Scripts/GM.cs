2747ce04411bfb9c94cef5a649ed2ef7bab485dc
HMu0510 <HMu0510@gmail.com>
2020-07-15 15:59:27 +0900

    NGUI Enemy
    - Enemy(이동, 충돌)
    - 충돌처리(플레이어 체력감소)

--- NGUIPractice/Assets/_LMH/Scripts/GM.cs
+++ NGUIPractice/Assets/_LMH/Scripts/GM.cs
@@ -6,13 +6,32 @@ public class GM : MonoBehaviour
 {
     [SerializeField] private GameObject bgSetObj;
     [SerializeField] private GameObject bgSetObj2;
+    [SerializeField] private GameObject enemySet1;
 
+    public List<GameObject> nowEnemyChild = new List<GameObject>();
+    public int enemyInt = 0;
+    public float yPos;
 
     public float moveSpeed;
     public float moveSpeed2;
+    public float maxSpeed;
 
     private float xPosMoveCheck = 0f;
     private float xPosMoveCheck2 = 0f;
+
+    public float xPosMoveChkVal1 = 0f;
+    public float xPosMoveChkVal2 = 0f;
+
+
+    public float timerLim;
+    public float timerForSpeed;
+
+    [SerializeField] UILabel Score;
+    public int gameScore;
+    public int gameScorePer;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,20 +41,71 @@ public class GM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        SpeedLimChk();
+
         xPosMoveCheck -= moveSpeed * Time.deltaTime;
         xPosMoveCheck2 -= moveSpeed2 * Time.deltaTime;
         bgSetObj.transform.localPosition -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
         bgSetObj2.transform.localPosition -= new Vector3(moveSpeed2 * Time.deltaTime, 0, 0);
-
+        enemySet1.transform.localPosition -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
         if (xPosMoveCheck <= -960)
         {
             xPosMoveCheck = 0;
             bgSetObj.transform.localPosition = new Vector3(960 * -1.0f, 0, 0);
+
+            gameScore += gameScorePer;
+            Score.text = gameScore.ToString();
         }
         if (xPosMoveCheck2 <= -960)
         {
             xPosMoveCheck2 = 0;
-            bgSetObj2.transform.localPosition = new Vector3(960 * -1.0f, -400, 0);
+            bgSetObj2.transform.localPosition = new Vector3(960* -1.0f, -400, 0);
+
+            if(enemySet1.transform.localPosition.x < xPosMoveChkVal1)
+            {
+                xPosMoveChkVal1 -= xPosMoveChkVal2;
+                ResetChildSet();
+                enemyInt++;
+                if(enemyInt >4)
+                {
+                    enemyInt = 0;
+                }
+            }
         }
+    }
+
+    private void SpeedLimChk()
+    {
+        timerForSpeed += Time.deltaTime;
+
+        if (timerForSpeed > timerLim)
+        {
+            timerForSpeed = 0;
+            if (moveSpeed < maxSpeed)
+            {
+                moveSpeed = moveSpeed * 1.1f;
+                moveSpeed2 = moveSpeed2 * 1.1f;
+            }
+        }
+    }
+    private void ResetChildSet()
+    {
+        nowEnemyChild[enemyInt].transform.localPosition += new Vector3(2880.0f, 0, 0);
+        switch(Random.Range(1,4))
+        {
+            case 1:
+                yPos = -150f;
+                break;
+            case 2:
+                yPos = 150.0f;
+                break;
+            case 3:
+                yPos = 300f;
+                break;
+        }
+        nowEnemyChild[enemyInt].transform.localPosition =
+            new Vector3(nowEnemyChild[enemyInt].transform.localPosition.x,
+            yPos, nowEnemyChild[enemyInt].transform.localPosition.z);
     }
 }
--- NGUIPractice/Assets/_LMH/Scripts/Player.cs
+++ NGUIPractice/Assets/_LMH/Scripts/Player.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float hp;
+    public float hpMax;
+    [SerializeField] GameObject GM;
+    public GM GMst;
+
+    [SerializeField] private UISprite hpBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = hpMax;
     }
 
     // Update is called once per frame
@@ -22,5 +28,25 @@ public class Player : MonoBehaviour
         {
             transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
         }
+
+        //TouchMove();
+
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        hp -= 10.0f;
+        hpBar.fillAmount = hp * 0.01f;
+        if(hp <=0)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("GameOver");
+        Time.timeScale = 0;
     }
 }
